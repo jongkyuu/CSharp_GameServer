22fcb92ada7e8875b73583cd4a349573a440980e
minerva421 <minerva421.ai@gmail.com>
2022-04-12 23:59:03 +0900

    메모리 베리어 추가중

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -6,10 +6,45 @@ namespace ServerCore
 {
     class Program
     {
+        // 하드웨어 최적화
+
+        static int x = 0;
+        static int y = 0;
+        static int r1 = 0;
+        static int r2 = 0;
+
+        static void Thread_1()
+        {
+            y = 1;
+            r1 = x;
+        }
+        static void Thread_2()
+        {
+            x = 1;
+            r2 = y;
+        }
 
         static void Main(string[] args)
         {
+            int count = 0;
 
+            while(true)
+            {
+                x = y = r1 = r2 = 0;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (r1 == 0 && r2 == 0)
+                    break;
+            }
+
+            Console.WriteLine($"{count}번 만에 빠져나옴");
         }
     }
 }
