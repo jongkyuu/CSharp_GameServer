330722311e463cf5e9fac1aa2313bbac045e9a1b
이종규 <jongkyu.lee@mirero.co.kr>
2022-04-18 12:34:52 +0900

    LockDemo 추가

--- InterlockedDemo/Program.cs
+++ InterlockedDemo/Program.cs
@@ -13,18 +13,18 @@ namespace InterlockedDemo
             for (int i = 0; i < 100000; i++)
             {
                 //number++;
-
+                
                 //int temp = number; // 0
                 //temp += 1;  // 1
                 //number = temp; // number = 1
+                // 위와 같은 상황에서 (특히 쓰기 상황) 여러 쓰레드들이 동시에 접근하면 문제가 되는 코드를 임계 영역(Critical Session)이라고 한다.
 
                 // 1 증가시키거나 감소시키는 연산을 한번에 수행해야 하는데 3단계에 걸쳐서 수행하기 때문에 문제가 생긴다
                 // atomic : 원자성, 어떤 동작이 한번에 일어나야 한다
 
-                Interlocked.Increment(ref number);
+                int afterValue = Interlocked.Increment(ref number);
+                // Interlocked 계열은 정수만 사용할 수 있다는 치명적인 단점이 있다
             }
-
-
         }
 
         static void Thread_2()
---
+++ LockDemo/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LockDemo
+{
+    class Program
+    {
+        private static int number = 0;
+        private static object _obj = new object();
+
+        static void Thread_1(){
+            for (int i = 0; i < 1000000; i++)
+            {
+
+                // 상호 배제(mutual exclusion, Mutex, 뮤텍스)
+                Monitor.Enter(_obj);  // 문을 잠그는 행위
+                {
+                    try
+                    {
+                        Monitor.Enter(_obj); // 직접 Monitor를 사용하는 경우는 거의 없다
+                        number++;
+
+                    }
+                    finally
+                    {
+                        Monitor.Exit(_obj);
+                    }
+                    //return;
+                    // Exception 발생 등..
+                    // 잠금을 열어주지 않고 나갈 경우에 문이 계속 잠겨있기 때문에 Thread_2는 계속 기다리게 된다. (DeadLock)
+
+                }
+                //Monitor.Exit(_obj);  // 잠금을 풀어준다
+            }
+        }
+
+        static void Thread_UseLock_1()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (_obj)
+                {
+                    number++;
+                }
+         
+            }
+        }
+
+        static void Thread_2()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                Monitor.Enter(_obj);
+
+                number--;
+
+                Monitor.Exit(_obj);
+            }
+        }
+
+        static void Thread_UseLock_2()
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                lock (_obj)
+                {
+                    number--;
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //Task t1 = new Task(Thread_1);
+            //Task t2 = new Task(Thread_2);
+
+            Task t1 = new Task(Thread_UseLock_1);
+            Task t2 = new Task(Thread_UseLock_2);
+
+            t1.Start();
+            t2.Start();
+
+            Task.WaitAll(t1, t2);
+
+            Console.WriteLine(number);
+
+        }
+    }
+}
