9e7dfa9478575499b9fd1181755dd0f72d8420c3
minerva421 <minerva421.ai@gmail.com>
2022-06-21 12:45:31 +0900

    PacketSession 문제 수정

--- Server/Program.cs
+++ Server/Program.cs
@@ -54,7 +54,7 @@ namespace Server
 
             Thread.Sleep(5000);
 
-            Disconnect();
+            //Disconnect();
         }
         public override void OnRecvPacket(ArraySegment<byte> buffer)
         {
